98e27980345cbdf269dc76c4e5ad70e871ed0fc2
Marc Godard <me@marcgodard.com>
2016-10-10 16:38:47 -0400

    Moved new test to the bottom as requested

--- src/mixins/test.js
+++ src/mixins/test.js
@@ -93,21 +93,7 @@ QUnit.test('Mixin child-rows', function(assert) {
   assert.equal(vm.attr('rows').filter(a => a.attr('childrenVisible')).length, 0, 'No rows should have children visible');
 });
 
-QUnit.test('Mixin pagination test 1', function(assert) {
-  var vm = new (can.Map.extend(mixinPagination))({
-    rows: _.times(24, i => i),
-    pagination: 25
-  });
-
-  // page 0:
-  assert.equal(vm.attr('rowsPerPage'), 25, 'rowsPerPage is 25');
-  assert.equal(vm.attr('currentPage'), 0, 'currentPage is 0');
-  assert.equal(vm.attr('totalPages'), 1, 'totalPages is 1');
-  assert.equal(vm.attr('hasPages'), false, 'Only 1 page, hide nav');
-});
-
 QUnit.test('Mixin pagination test 2', function(assert) {
-
   var vm = new (can.Map.extend(mixinPagination))({
     rows: _.times(24, i => i),
     pagination: 10
@@ -147,3 +133,15 @@ QUnit.test('Mixin pagination test 2', function(assert) {
   assert.equal(vm.attr('currentPage'), 1, 'Change page to 2');
 });
 
+QUnit.test('Mixin pagination test 2', function(assert) {
+  var vm = new (can.Map.extend(mixinPagination))({
+    rows: _.times(24, i => i),
+    pagination: 25
+  });
+
+  // page 0:
+  assert.equal(vm.attr('rowsPerPage'), 25, 'rowsPerPage is 25');
+  assert.equal(vm.attr('currentPage'), 0, 'currentPage is 0');
+  assert.equal(vm.attr('totalPages'), 1, 'totalPages is 1');
+  assert.equal(vm.attr('hasPages'), false, 'Only 1 page, hide nav');
+});
